a543adbcc363a78fee8532d274ccde24bd8b9eb8
Simon Cadge <SimonHCadge@gmail.com>
2018-10-27 21:20:07 +0100

    Huzzah data! #cowboy

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -1,6 +1,28 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
+    selector: 'app-root',
+    template: `
+        <ul>
+            <li *ngFor="let item of items | async">
+                <pre>{{ item | json }}</pre>
+            </li>
+        </ul>
+    `
+})
+export class AppComponent {
+    public items: Observable<any[]>;
+
+    constructor(db: AngularFirestore) {
+        this.items = db.collection('/items').valueChanges();
+    }
+}
+
+
+/*@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -8,3 +30,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Team3App';
 }
+*/
--- Angular/my-app/src/main.ts
+++ Angular/my-app/src/main.ts
@@ -1,9 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
