19d1ae96ada7816e56ff82608e09d0729c6b01df
Simon Cadge <SimonHCadge@gmail.com>
2018-10-28 00:28:32 +0100

    Stand and deliver my responsible home owner you scoundrel

--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -4,16 +4,32 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { ForumComponent } from './forum/forum.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'forum', component: ForumComponent},
+
+  {path: '**', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    ForumComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
