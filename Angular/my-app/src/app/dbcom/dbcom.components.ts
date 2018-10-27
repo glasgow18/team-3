179b214d19611fa6b66e6aba68756b053be769af
Zyxwee <jelm@st-andrews.ac.uk>
2018-10-28 00:17:48 +0100

    #TheyWereAForceForGoodInAHubOfRealGits

---
+++ Angular/my-app/src/app/dbcom/dbcom.components.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+// Gonna write to the db (doctrinal basis obvs)
+
+// Highlights part of the page
+@Component({
+    // the tag to select
+    selector: 'app-root',
+    // from file
+    templateUrl: './app.component.html',
+    // with style
+    styleUrls: ['./app.component.css']
+})
+export class dbcom {
+
+}
