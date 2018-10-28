bf56bb02e333a9a72dde68bd602bea9f78bb4455
eddie9802 <ehamilton9810@gmail.com>
2018-10-28 09:31:18 +0000

    Pressure! Pushing down on me!

--- MessengerReceiver.js
+++ MessengerReceiver.js
@@ -1,6 +1,7 @@
 $('.submitbutton').on("click", function() {
 var input = $('.submitinput').val();
-var userMessage = {user: "you", message: input.toString()};
+var userMessage = [{user: "you", message: input.toString()}];
+console.log(userMessage);
 displayMessage(userMessage);
 $('.submitinput').val("");
 });
